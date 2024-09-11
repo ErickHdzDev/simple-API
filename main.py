c8cbafaf3c78e063e8294e76a37fbadf1fa6bb31
Erick Hernandez MAC <erick.hernandez@g-tech.io>
2024-09-11 15:00:32 -0600

    se agrego la instruccion de clonar el repositorio de git antes de hacer el build

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ import platform
 app = FastAPI()
 
 @app.get("/")
-def echo():
+def hello():
     return {"body":f"Hello from {platform.node()}"}
+
+@app.post("/echo")
+def echo(body:dict):
+    msg = body.get("msg", None)
+    if not msg:
+        return {"body":"msg is required"}
+    
+    return {"body":f"Message: {msg}"}
